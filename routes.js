23251ef52e2d458926ec191bc53bd512a91df87d
Nelson Cardenas <nelsonjc.01@gmail.com>
2016-03-30 18:56:11 -0600

    routes

--- routes.js
+++ routes.js
@@ -3,8 +3,8 @@ var usersController = require('./controllers/usersController');
 var authController = require('./controllers/authController');
 
 exports.endpoints = [{method: 'GET', path: '/', config: {handler: function(request, reply){reply('API v1, offers')}}},
-	{method: 'GET', path: '/v1/offers', config: offersController.getoffers},
-  {method: 'POST', path: '/v1/offer', config: offersController.createoffer},
+	{method: 'GET', path: '/v1/offers', config: offersController.getOffers},
+  {method: 'POST', path: '/v1/offer', config: offersController.createOffer},
 	{method: 'POST', path: '/v1/register', config: usersController.createUser},
 	{method: 'POST', path: '/v1/login', config: authController.login},
 	{method: 'GET', path: '/v1/logout', config: authController.logout}
